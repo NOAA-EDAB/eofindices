f5f916bdfe44437e511c0d5efbe1ebacc08b9ee6
andybeet <22455149+andybeet@users.noreply.github.com>
2020-01-15 12:05:12 -0500

    Renamed calc_mtl_index

--- R/calc_mtl_index.R
+++ R/calc_mtl_index.R
@@ -5,17 +5,20 @@
 #'
 #'@param data dataframe. n x anything. Data containing the total catch by each species in each year. A complete grid of year by species must be passed. Columns must be named YEAR,NESPP3,catch.
 #'@param speciesTL dataframe. m x anything. Species codes and trophic level for the most abundantly caught species. One column must be named NESPP3 and another Troph
-#'@speciesCode Character string. The name of the column that holds the speciesCodes. Default = "NESPP3".
+#'@param speciesCode Character string. The name of the column that holds the speciesCodes. Default = "NESPP3".
 #'
+#'@return Data frame:
 #'
-#'@return
+#'\item{Year}{Year of the index}
+#'\item{Index}{Value of the index}
+#'
 #'
 #'
 #'@export
 
 ## need to generalize column names
 
-calc_mean_trophic_level <- function(catch,speciesTL,speciesCode="NESPP3"){
+calc_mtl_index <- function(catch,speciesTL,speciesCode="NESPP3"){
 
   #preallocate dataframe
   years <- unique(catch$YEAR)
@@ -34,7 +37,7 @@ calc_mean_trophic_level <- function(catch,speciesTL,speciesCode="NESPP3"){
     # decision rule for missing data. ALL to zero
     master$totLand[is.na(master$totLand)] <- 0
     # index
-    meanTL$MTL[iy] <- sum(master$totLand * master$Troph) / sum(master$totLand)
+    meanTL$Index[iy] <- sum(master$totLand * master$Troph) / sum(master$totLand)
   }
 
   return(meanTL)
