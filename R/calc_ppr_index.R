b836826318dc6ccbf3d95a539191af408ca2654d
andybeet <22455149+andybeet@users.noreply.github.com>
2020-01-15 12:07:28 -0500

    changed output format and updated help

--- R/calc_ppr_index.R
+++ R/calc_ppr_index.R
@@ -9,8 +9,12 @@
 #'Columns must be named YEAR,NESPP3,catch
 #'@param speciesTL dataframe. m x anything. Species codes and trophic level for the most abundantly caught species.
 #'One column must be named NESPP3 and another Troph
+#'@param speciesCode Character string. The name of the column that holds the speciesCodes. Default = "NESPP3".
 #'
-#'@return
+#'@return Data frame:
+#'
+#'\item{Year}{Year of the index}
+#'\item{Index}{Value of the index}
 #'
 #'
 #'@export
@@ -36,7 +40,7 @@ calc_ppr_index <- function(catch,speciesTL,speciesCode = "NESPP3"){
     # decision rule for missing data. ALL to zero
     master$totLand[is.na(master$totLand)] <- 0
     # index
-    PPR$PPR[iy] <- sum((master$totLand/9)*10^(master$Troph-1))
+    PPR$Index[iy] <- sum((master$totLand/9)*10^(master$Troph-1))
   }
 
   return(PPR)
