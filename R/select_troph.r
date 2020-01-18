0b6c130008f44de1fb8f5df12c525c44a07c3f15
andybeet <22455149+andybeet@users.noreply.github.com>
2020-01-17 21:11:46 -0500

    use EstTroph as a backup to FoodTroph and DietTroph

--- R/select_troph.r
+++ R/select_troph.r
@@ -1,19 +1,25 @@
 #'Combine trophic level data
 #'
 #'Fishbase contains Trophic level data in several locations.
-#'We use the field: DietTroph as our main measure of Trophic Level.
-#'If this is absent we resort to the field: FoodTroph
+#'We use the field: DietTroph as our main measure of Trophic Level (ecology table)
+#'If this is absent we resort to the field: FoodTroph (ecology table)
+#'then finally EstTroph (estimate table)
 #'
 #'@param DietTroph Numeric vector. Value of Trophic level
 #'@param FoodTroph Numeric vector. Value of Trophic level
+#'@param EstTroph Numeric vector. Value of Trophic level
 #'
 #'@return Numeric vector. combined vectors of Trophic level
 #'@export
 
 
-select_troph <- function(DietTroph,FoodTroph){
+select_troph <- function(DietTroph,FoodTroph,EstTroph){
   troph <- DietTroph
+  # if Dietroph is missing use FoodTroph
   ind <- is.na(DietTroph)
   troph[ind] <- FoodTroph[ind]
+  # if still missing then use EstTroph
+  ind <- is.na(DietTroph)
+  troph[ind] <- EstTroph[ind]
   return(troph)
 }
