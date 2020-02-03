ff62c00e56b364f27dfb37be043600087b32cf0e
andybeet <22455149+andybeet@users.noreply.github.com>
2020-02-03 16:55:28 -0500

    add PP data + - function to scale PPR index by PP - get PP data

---
+++ R/calc_PPR_scaled.R
@@ -0,0 +1,31 @@
+#' Scale PPR index by toal production
+#'
+#'
+#'
+#'@param PPR dataframe (nx2). Primary production required index over time. Two columns YEAR and INDEX
+#'@param PP list.
+#'
+#'@return dataframe.
+#'
+#'
+#'@export
+
+
+calc_PPR_scaled <- function(PPR,PP,units="m") {
+
+  if (tolower(units)=="m") { # convert from km2 to m2 (multiply by 10^6)
+    epuAreaM2 <- PP$EPUarea*1000000
+  }
+
+  print(epuAreaM2)
+  #print(epuAreaM2OLD)
+
+  joinedTab <- dplyr::left_join(PP$PP,PPR,by = "YEAR")
+
+  scaledIndex <- joinedTab %>%
+    dplyr::mutate(scaled = INDEX/(365*epuAreaM2)) %>%
+    dplyr::mutate(SCALEDINDEX = scaled/ANNUAL_MEAN)
+
+  return(scaledIndex)
+
+}
