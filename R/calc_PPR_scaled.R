b0be0f3d212092b9431e4040c60aa6801fdea343
andybeet <22455149+andybeet@users.noreply.github.com>
2021-02-18 16:46:05 -0500

    Added 2021 example vignete

--- R/calc_PPR_scaled.R
+++ R/calc_PPR_scaled.R
@@ -9,6 +9,7 @@
 #'
 #'@param PPR Data frame (n x 2). Primary production required index over time. Two columns YEAR and INDEX
 #'@param PP List. First element labeled \code{PP} is a Data frame of size (n x 2). Columns YEAR, ANNUAL_MEAN. Second element labeled \code{EPUarea} is the area of the region
+#'@param pprField Character string. The name of the field in \code{PPR} which contains the PPR index data.
 #'@param yearFieldPPR Character string. The name of the field in \code{PPR} which contains the Year.
 #'@param yearFieldPP Character string. The name of the field in \code{PP} which contains the Year.
 #'@param ppField Character string. The name of the field in \code{PP} which contains the PP data.
